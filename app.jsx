f7fd53be31c9d23aedf5713b167c57a698b3641f
opaloutfittersofficial-eng <opal.outfittersofficial@gmail.com>
2026-03-26 22:18:12 -0700

    Create  app.jsx

---
+++ app.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar'; 
+import Footer from './components/Footer';
+import WhatsAppButton from './components/WhatsAppButton';
+import Home from './pages/Home';
+import ShoePage from './pages/ShoePage';
+import FashionPage from './pages/FashionPage';
+import NotFound from './pages/NotFound';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+     <div className="min-h-screen flex flex-col bg-[#0a1f0a]">
+         <Navbar />
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/shoes/:id" element={<ShoePage />} />
+            <Route path="/fashion/:category" element={<FashionPage />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+        <WhatsAppButton />
+      </div>
+    </BrowserRouter>
+  );
+} 
